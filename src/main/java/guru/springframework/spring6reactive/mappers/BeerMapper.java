d13ac5370a674d7848b0ad964c992f37e939a493
Rob van Dijk <rob@optimust.nl>
2024-02-07 22:20:59 +0100

    Opdracht 27: Assn - Implement MapStruct Mappers

---
+++ src/main/java/guru/springframework/spring6reactive/mappers/BeerMapper.java
@@ -0,0 +1,15 @@
+package guru.springframework.spring6reactive.mappers;
+
+import guru.springframework.spring6reactive.domain.Beer;
+import guru.springframework.spring6reactive.model.BeerDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface BeerMapper {
+
+    Beer beerDtoToBeer(BeerDTO dto);
+
+    BeerDTO beerToBeerDto(Beer beer);
+
+}
