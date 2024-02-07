16e29b20d0f4b2bb1d61bc184625a5c05cc1c136
Rob van Dijk <rob@optimust.nl>
2024-02-07 23:29:24 +0100

    273. Create New Beer Entity

--- src/main/java/guru/springframework/spring6reactive/controllers/BeerController.java
+++ src/main/java/guru/springframework/spring6reactive/controllers/BeerController.java
@@ -3,10 +3,9 @@ package guru.springframework.spring6reactive.controllers;
 import guru.springframework.spring6reactive.model.BeerDTO;
 import guru.springframework.spring6reactive.services.BeerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -27,6 +26,15 @@ public class BeerController {
     @GetMapping(BEER_PATH_ID)
     Mono<BeerDTO> getBeerById(@PathVariable("beerId") Integer beerId) {
         return beerService.getBeerById(beerId);
+    }
+
+    @PostMapping(BEER_PATH)
+    Mono<ResponseEntity<Void>> createNewBeer(BeerDTO beerDTO) {
+        return beerService.saveNewBeer(beerDTO)
+                .map(savedDto -> ResponseEntity.created(UriComponentsBuilder
+                                .fromHttpUrl("http://localhost:8080/" + BEER_PATH + "/" + savedDto.getId())
+                                .build().toUri())
+                        .build());
     }
 
 }
