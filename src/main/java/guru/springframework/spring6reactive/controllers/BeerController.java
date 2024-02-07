21c210bd8f4fffb896e702e91fe489e019f906d6
Rob van Dijk <rob@optimust.nl>
2024-02-07 22:40:13 +0100

    271. List Flux from Database

--- src/main/java/guru/springframework/spring6reactive/controllers/BeerController.java
+++ src/main/java/guru/springframework/spring6reactive/controllers/BeerController.java
@@ -1,18 +1,23 @@
 package guru.springframework.spring6reactive.controllers;
 
 import guru.springframework.spring6reactive.model.BeerDTO;
+import guru.springframework.spring6reactive.services.BeerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
+@RequiredArgsConstructor
 public class BeerController {
 
     public static final String BEER_PATH = "/api/v2/beer";
 
+    private final BeerService beerService;
+
     @GetMapping(BEER_PATH)
     Flux<BeerDTO> listBeers() {
-        return Flux.just(BeerDTO.builder().id(1).build(), BeerDTO.builder().id(2).build());
+        return beerService.listBeers();
     }
 
 }
