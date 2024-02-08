d5ac3904344b02a62d83a5fbdc4f1cfb33ba625a
Rob van Dijk <rob@optimust.nl>
2024-02-08 21:46:16 +0100

    287. Test Validation Failure On Create

--- src/test/java/guru/springframework/spring6reactive/controllers/CustomerControllerTest.java
+++ src/test/java/guru/springframework/spring6reactive/controllers/CustomerControllerTest.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
